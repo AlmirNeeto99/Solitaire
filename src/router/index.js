cd1c8c2b35ad40b7ad92f942bad7d7bdb1cbc497
Almir Neto <almirneto338@gmail.com>
2021-02-15 21:24:52 -0300

    defining menu route and guard

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,25 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    name: 'Menu',
+    path: '/',
+    component: () => import(/* webpackChunkName: "menu" */ '../views/Menu.vue'),
+    meta: {
+      title: 'Menu',
+    },
+  },
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes,
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = to.meta.title + ' - ' + Vue.prototype.$appName
+  next()
 })
 
 export default router
