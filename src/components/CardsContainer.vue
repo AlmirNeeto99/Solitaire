aa652913b10c38d47ed20772c3ecf370507a4f64
Almir Neto <almirneto338@gmail.com>
2021-02-18 09:37:50 -0300

    creating the cards container

---
+++ src/components/CardsContainer.vue
@@ -0,0 +1,7 @@
+<template>
+  <div
+    class="h-44 w-32 bg-transparent shadow-lg border border-gray-200 flex justify-center items-center rounded-md"
+  >
+    <i class="mdi mdi-circle-outline text-5xl text-gray-200"></i>
+  </div>
+</template>
