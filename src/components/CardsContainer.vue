65cfd593204d3275e6b9fa39fec2cc6045080af0
Almir Neto <almirneto338@gmail.com>
2021-02-22 23:48:24 -0300

    added container slot to keep cards added relative class

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,7 +1,18 @@
 <template>
   <div
-    class="h-44 w-32 bg-transparent shadow-lg border border-gray-200 flex justify-center items-center rounded-md"
+    class="h-44 w-32 bg-transparent shadow-lg border border-gray-200 flex justify-center items-center rounded-md relative"
   >
-    <i class="mdi mdi-circle-outline text-5xl text-gray-200"></i>
+    <i class="mdi mdi-circle-outline text-5xl text-gray-200 absolute"></i>
+    <slot></slot>
   </div>
 </template>
+
+<script>
+export default {
+  mounted(){
+    this.$el.addEventListener("mouseup", function(e){
+      console.log(e)
+    })
+  }
+}
+</script>
