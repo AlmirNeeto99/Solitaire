5423cdf693e1ca04f19fcda2a7ce0f5276131b7d
Almir Neto <almirneto338@gmail.com>
2021-02-24 09:15:16 -0300

    added cards mutation getter and action

--- src/store/index.js
+++ src/store/index.js
@@ -6,13 +6,31 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     hasSavedGame: false,
+    cards: [],
   },
   getters: {
     hasSavedGame(state) {
       return state.hasSavedGame
     },
+    cards(state) {
+      return state.cards
+    },
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    /**
+     * Mutation that sets cards' array
+     * @param {Object} state Application state
+     * @param {Array} cards New cards array
+     */
+    setCards(state, cards) {
+      state.cards = cards
+      console.log(state.cards)
+    },
+  },
+  actions: {
+    setCards({ commit }, cards) {
+      commit('setCards', cards)
+    },
+  },
   modules: {},
 })
