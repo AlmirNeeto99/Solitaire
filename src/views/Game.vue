504a2ecf48b73e2397162fdcdbb553e83cab6719
Almir Neto <almirneto338@gmail.com>
2021-02-24 09:16:27 -0300

    creating the cards, shuffling them and putting to a container

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -5,20 +5,28 @@
       class="w-full flex flex-wrap h-full py-3 px-3 flex-row justify-between"
     >
       <div id="stock" class="h-44 w-32">
-        <cards-container><card rank="A" suit="hearts" /></cards-container>
+        <cards-container></cards-container>
       </div>
       <div id="foundations" class="h-44 w-3/5 flex justify-between">
-        <cards-container><card rank="A" suit="hearts" /></cards-container>
-        <cards-container><card rank="A" suit="clubs" /></cards-container>
-        <cards-container><card rank="A" suit="diamonds" /></cards-container>
-        <cards-container><card rank="A" suit="spades" /></cards-container>
+        <cards-container></cards-container>
+        <cards-container></cards-container>
+        <cards-container></cards-container>
+        <cards-container></cards-container>
       </div>
     </div>
     <div
       id="lower"
       class="w-full flex justify-between p-3 border-t border-gray-200"
     >
-      <cards-container></cards-container>
+      <cards-container>
+        <card
+          v-for="(card, index) in cards"
+          :suit="card.suit"
+          :rank="card.rank"
+          :key="index"
+          :index="index"
+        />
+      </cards-container>
       <cards-container></cards-container>
       <cards-container></cards-container>
       <cards-container></cards-container>
@@ -34,6 +42,40 @@ import CardsContainer from '@/components/CardsContainer'
 import Card from '@/components/Card'
 export default {
   components: { CardsContainer, Card },
+  computed: {
+    cards() {
+      return this.$store.state.cards
+    },
+  },
+  created() {
+    const suits = ['diamonds', 'hearts', 'spades', 'clubs']
+    const ranks = [
+      'A',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      '10',
+      'J',
+      'Q',
+      'K',
+    ]
+    let cards = []
+    suits.forEach((suit) => {
+      ranks.forEach((rank) => {
+        cards.push({
+          rank: rank,
+          suit: suit,
+        })
+      })
+    })
+    cards = this._.shuffle(cards)
+    this.$store.dispatch('setCards', cards)
+  },
 }
 </script>
 
