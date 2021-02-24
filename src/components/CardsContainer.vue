55096214241b577fffa018af5329457c509bdc65
Almir Neto <almirneto338@gmail.com>
2021-02-24 08:11:06 -0300

    adding container class

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,18 +1,8 @@
 <template>
   <div
-    class="h-44 w-32 bg-transparent shadow-lg border border-gray-200 flex justify-center items-center rounded-md relative"
+    class="h-44 cards-container w-32 bg-transparent shadow-lg border border-gray-300 flex justify-center items-center rounded-md relative"
   >
     <i class="mdi mdi-circle-outline text-5xl text-gray-200 absolute"></i>
     <slot></slot>
   </div>
 </template>
-
-<script>
-export default {
-  mounted(){
-    this.$el.addEventListener("mouseup", function(e){
-      console.log(e)
-    })
-  }
-}
-</script>
