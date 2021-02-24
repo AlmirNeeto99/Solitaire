54798e72f1bc6b8281dcc6a57da888d4b0568e94
Almir Neto <almirneto338@gmail.com>
2021-02-24 09:16:04 -0300

    adding the empty indicator when no slot are present

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,8 +1,12 @@
 <template>
   <div
-    class="h-44 cards-container w-32 bg-transparent shadow-lg border border-gray-300 flex justify-center items-center rounded-md relative"
+    class="h-44 cards-container w-32 bg-transparent shadow-lg border border-gray-300 flex rounded-md relative items-center flex-col"
+    :class="{ 'justify-center': !this.$slots.default }"
   >
-    <i class="mdi mdi-circle-outline text-5xl text-gray-200 absolute"></i>
+    <i
+      class="mdi mdi-circle-outline text-5xl text-gray-200 absolute"
+      v-if="!this.$slots.default"
+    ></i>
     <slot></slot>
   </div>
 </template>
