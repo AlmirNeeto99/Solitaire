9dd884c4659ccf07aa532522a2b0abb1ff1e236f
Almir Neto <almirneto338@gmail.com>
2021-02-27 23:26:56 -0300

    adding transition thought pages

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="min-h-screen">
+  <div class="min-h-screen relative">
     <transition name="fade">
-      <router-view class="min-h-screen flex flex-wrap relative" />
+      <router-view class="min-h-screen flex flex-wrap" />
     </transition>
   </div>
 </template>
@@ -19,4 +19,13 @@ body {
 <style lang="scss">
 $mdi-font-path: '~@mdi/font/fonts';
 @import '~@mdi/font/scss/materialdesignicons';
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 </style>
