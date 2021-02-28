9d5ba5330a62011fbae18df5774330af0432a149
Almir Neto <almirneto338@gmail.com>
2021-02-27 23:29:14 -0300

    removing classes

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="h-44 cards-container w-32 bg-transparent shadow-lg border border-gray-300 flex rounded-md relative items-center flex-col"
+    class="h-44 cards-container w-32 bg-transparent shadow-lg border border-gray-300 flex rounded-md items-center flex-col"
     :class="{ 'justify-center': !this.$slots.default }"
   >
     <i
