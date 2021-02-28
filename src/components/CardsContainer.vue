a8cf4be7cf13b7c9e5dbefdb1e78b7d7ac1bfdfb
Almir Neto <almirneto338@gmail.com>
2021-02-28 01:18:47 -0300

    added the logic to pile cards (stock)

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,12 +1,35 @@
 <template>
   <div
     class="h-44 cards-container w-32 bg-transparent shadow-lg border border-gray-300 flex rounded-md items-center flex-col"
-    :class="{ 'justify-center': !this.$slots.default }"
+    :class="{ 'justify-center': this.cards.length == 0 }"
   >
     <i
       class="mdi mdi-circle-outline text-5xl text-gray-200 absolute"
-      v-if="!this.$slots.default"
+      v-if="this.cards.length == 0"
     ></i>
-    <slot></slot>
+    <card
+      v-for="(card, index) in this.cards"
+      :suit="card.suit"
+      :rank="card.rank"
+      :key="index"
+      :index="index"
+      :down="card.down"
+      :pile="shouldPile"
+    />
   </div>
 </template>
+
+<script>
+import Card from '@/components/Card'
+export default {
+  props: ['cards', 'pile'],
+  components: {
+    Card,
+  },
+  computed: {
+    shouldPile() {
+      return this.pile !== undefined ? this.pile : false
+    },
+  },
+}
+</script>
